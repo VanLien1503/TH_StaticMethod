43c63017bceeb8b723c1082f42382a3c023dee92
VanLien1503 <hoanglien89nd@gmail.com>
2020-05-02 17:52:00 +0700

    TH

---
+++ StaticMothod.java
@@ -0,0 +1,14 @@
+package TH_StaticMethod;
+
+public class StaticMothod {
+    public static class Student{
+        private int rollno;
+        private String name;
+        private static String college="BBDIT";
+    }
+    public static class TestStaticMethod{
+        public static void main(String[] args) {
+            
+        }
+    }
+}
